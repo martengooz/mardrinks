0107f5e5340ae1e08aafc43ff64ed1817d956880
snobben <martengooz@gmail.com>
2019-01-02 22:47:45 +0100

    Working Itemgrid

--- src/components/ItemGrid.vue
+++ src/components/ItemGrid.vue
@@ -1,30 +1,31 @@
 <template>
-  <v-layout justify-center>
-    <v-flex xs12 sm12 md12 lg12 >
-        <v-container v-bind="{ [`grid-list-md`]: true }" fluid>
-          <v-layout row wrap>
-            <v-flex
-              v-for="n in 20"
-              :key="n"
-              xs6 sm4 md3 lg2
-            >
-
-            <ItemCard/>
-            </v-flex>
-          </v-layout>
-        </v-container>
-    </v-flex>
-  </v-layout>
+  <v-container v-bind="{ [`grid-list-md`]: true }" fluid>
+    <v-layout row wrap>
+      <v-flex
+        v-for="item in items"
+        :key="item.id"
+        xs6 sm4 md3 lg2
+      >
+        <ItemCard :name="item.name" :img="item.img" />
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
   import ItemCard from '@/components/ItemCard.vue'
 
   export default {
+    name: "ItemGrid",
     data: () => ({
     }),
+    props: {
+      category: String,
+      items: {}
+    },
     components: {
       ItemCard
     }
   }
+
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,33 @@
 <template>
   <v-container fluid fill-height class="grey lighten-4">
-    <ItemGrid/>
+    <v-layout justify-center>
+      <v-flex xs12 sm12 md8 lg8 >
+        <div row v-for="(itemList, category) in Items">
+          <ItemGrid :category="category" :items="itemList" />
+        </div>
+      </v-flex>
+
+      <v-flex xs0 sm0 md4 lg4 >
+        <ItemDetails :id="'asd'" :name="'Drink'"/>
+    </v-flex>
+  </v-layout>
   </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
 import ItemGrid from '@/components/ItemGrid.vue'
+import ItemDetails from '@/components/ItemDetails.vue'
+import { Items } from '@/items.js'
 
 export default {
   name: 'home',
+  data: () => ({
+    Items : Items
+  }),
   components: {
-    ItemGrid
+    ItemGrid,
+    ItemDetails
   }
 }
 </script>
