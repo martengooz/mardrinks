558647203ef984826fa2dd5d969db91b57a01bbc
snobben <martengooz@gmail.com>
2019-01-05 12:41:59 +0100

    Update drink list

--- src/items.js
+++ src/items.js
@@ -4,119 +4,27 @@ export const Items = {
       id: 1,
       name: "Vodka virgin",
       img: "/img/1.png",
+      description : "Vanligt jävla vatten.",
       ingredients: [
         {'Is' : '2 bitar'},
         {'Vatten' : '40 cl'}
       ]
     },{
       id: 2,
-      name: "Vodka virgin",
+      name: "Other drink",
       img: "/img/2.png",
+      description : "En drink med massa sprit och stuff.",
       ingredients: [
-        {'Is' : '2 bitar'},
-        {'Vatten' : '40 cl'}
-      ]
-    },
-    {
-      id: 3,
-      name: "Vodka virgin",
-      img: "/img/3.png",
-      ingredients: [
-        {'Is' : '2 bitar'},
-        {'Vatten' : '40 cl'}
-      ]
-    },
-    {
-      id: 4,
-      name: "Vodka virgin",
-      img: "/img/4.jpeg",
-      ingredients: [
-        {'Is' : '2 bitar'},
-        {'Vatten' : '40 cl'}
-      ]
-    },
-    {
-      id: 5,
-      name: "Vodka virgin",
-      img: "/img/5.png",
-      ingredients: [
-        {'Is' : '2 bitar'},
-        {'Vatten' : '40 cl'}
-      ]
-    },
-    {
-      id: 6,
-      name: "Vodka virgin",
-      img: "/img/6.png",
-      ingredients: [
-        {'Is' : '2 bitar'},
-        {'Vatten' : '40 cl'}
-      ]
-    },
-    {
-      id: 7,
-      name: "Vodka virgin",
-      img: "/img/7.jpg",
-      ingredients: [
-        {'Is' : '2 bitar'},
-        {'Vatten' : '40 cl'}
-      ]
-    },
-    {
-      id: 8,
-      name: "Vodka virgin",
-      img: "/img/9.jpeg",
-      ingredients: [
-        {'Is' : '2 bitar'},
-        {'Vatten' : '40 cl'}
-      ]
-    },
-    {
-      id: 9,
-      name: "Vodka virgin",
-      img: "/img/10.jpeg",
-      ingredients: [
-        {'Is' : '2 bitar'},
-        {'Vatten' : '40 cl'}
-      ]
-    },{
-      id: 10,
-      name: "Vodka virgin",
-      img: "/img/11.jpg",
-      ingredients: [
-        {'Is' : '2 bitar'},
-        {'Vatten' : '40 cl'}
-      ]
-    },
-    {
-      id: 11,
-      name: "Vodka virgin",
-      img: "/img/12.jpeg",
-      ingredients: [
-        {'Is' : '2 bitar'},
-        {'Vatten' : '40 cl'}
-      ]
-    },{
-      id: 12,
-      name: "Vodka virgin",
-      img: "/img/13.jpeg",
-      ingredients: [
-        {'Is' : '2 bitar'},
-        {'Vatten' : '40 cl'}
+        {'Gin' : '1 L'},
+        {'Bols Blue' : '4 cl'},
+        {'Bols Blue' : '4 cl'},
+        {'Bols Red' : '4 cl'}
       ]
     }
   ],
   Gin: [
     {
       id: 1,
-      name: "Gin virgin",
-      img: "https://unsplash.it/150/300?image=11",
-      ingredients: [
-        {'Is' : '2 bitar'},
-        {'Vatten' : '40 cl'}
-      ]
-    },{
-      id: 2,
       name: "Gin virgin",
       img: "https://unsplash.it/150/300?image=11",
       ingredients: [
